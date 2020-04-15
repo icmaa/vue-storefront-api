2d34a17ea8f9958509f1b006e987b1a73d43f263
Christian Ewald <c.ewald@impericon.com>
2020-04-15 07:26:14 +0200

    #197501 Improve `SearchQueryText` category-aggregation in `icmaa-catalog` module (#61)
    * Add a sorting by `scoring` first and then by `doc_count` to  category aggregations and also increase the limit to 50

--- src/api/extensions/icmaa-catalog/filter/catalog/SearchTextQuery.ts
+++ src/api/extensions/icmaa-catalog/filter/catalog/SearchTextQuery.ts
@@ -5,8 +5,8 @@ import getFunctionScores from 'storefront-query-builder/lib/elasticsearch/score'
 
 const filter: FilterInterface = {
   priority: 1,
-  check: ({ attribute }) => attribute === 'search-text',
-  filter ({ queryChain, value }) {
+  check: ({ attribute }) => ['search-text', 'search-text-plain'].includes(attribute),
+  filter ({ queryChain, value, attribute }) {
     /**
      * This is a modified copy of the `applyTextQuery()` method in `storefront-query-builder/src/elasticsearch/body.ts`.
      * We added support for mutlimatch the nested category property. See `README.md` for more info.
@@ -38,11 +38,16 @@ const filter: FilterInterface = {
     }
 
     // Add category-aggregation using `nested` and `top-hits` to get all possible categories in results for category filter
-    this.queryChain.agg('nested', { path: 'category' }, 'categories_found', b => {
-      return b.agg('terms', 'category.category_id', {}, 'categories', c => {
-        return c.agg('top_hits', { _source: [ 'category.name', 'category.category_id', 'category.position' ], size: 1 }, 'hits')
+    // And sort them by their max score first and then by documents found with this category
+    if (!attribute.endsWith('plain')) {
+      this.queryChain.agg('nested', { path: 'category' }, 'categories_found', b => {
+        const options = { size: 50, order: [ { 'max_score': 'desc' }, { '_count': 'desc' } ] }
+        return b.agg('terms', 'category.category_id', options, 'categories', c => {
+          return c.agg('max', { script: '_score' }, 'max_score')
+            .agg('top_hits', { _source: [ 'category.name', 'category.category_id', 'category.position' ], size: 1 }, 'hits')
+        })
       })
-    })
+    }
 
     return this.queryChain
   },
