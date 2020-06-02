fd3aaa79ff7c48bb1e766c93b6d9912828a82e59
Christian Ewald <c.ewald@impericon.com>
2020-06-02 15:15:07 +0200

    Hotfix for promise rejection exception in `icmaa-spotify` module (#77)

--- src/api/extensions/icmaa-spotify/index.js
+++ src/api/extensions/icmaa-spotify/index.js
@@ -30,27 +30,27 @@ module.exports = ({ config }) => {
     const artistId = await Axios.get(apiUrl + '/search', {
       headers: { 'Authorization': `Bearer ${accessToken}` },
       params: { q: name, type: 'artist' }
-    }).then(response => {
-      if (response.data.artists.items.length > 0) {
-        return response.data.artists.items[0].id
-      }
+    }).then(resp => resp.data.artists.items.length > 0 ? resp.data.artists.items[0].id : false)
+      .catch(err => ({ error: err.response.data }))
 
+    if (!artistId) {
       return apiStatus(res, `Artist not found`, 400)
-    })
+    } else if (artistId.hasOwnProperty('error')) {
+      return apiStatus(res, artistId, 400)
+    }
 
-    const relatedArtists = await Axios.get(apiUrl + `/artists/${artistId}/related-artists`, {
+    return Axios.get(apiUrl + `/artists/${artistId}/related-artists`, {
       headers: { 'Authorization': `Bearer ${accessToken}` }
     }).then(resp => {
       if (resp.data.artists.length > 0) {
-        return resp.data.artists
+        const relatedArtists = resp.data.artists
           .sort((a, b) => a.popularity > b.popularity ? -1 : 1)
           .map(artist => artist.name)
+        return apiStatus(res, relatedArtists, 200)
       }
 
       return apiStatus(res, `No related artists not found`, 400)
-    })
-
-    return apiStatus(res, relatedArtists, 200)
+    }).catch(err => apiStatus(res, err.response.data, 400))
   })
 
   return api
